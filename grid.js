8140b85ecb08c0496c633e4336ae4fd1e03fe9e4
Mayank Mani <many.mayank@gmail.com>
2020-06-29 23:35:20 +0530

    finished

--- grid.js
+++ grid.js
@@ -45,6 +45,15 @@ function createGrid() {
 		const row = $('<div></div>').attr('class', 'graph-row');
 		// row.css('height', h);
 		for (var j = 0; j < columns; j++) {
+			if (i == 4 && j == 4) {
+				const startBox = `<div class="graph-col start" draggable><i class="fas fa-star-of-life"></i></div>`;
+				row.append(startBox);
+				continue;
+			} else if (i == 15 && j == 15) {
+				const endBox = `<div class="graph-col end" draggable><i class="far fa-dot-circle"></i></div>`;
+				row.append(endBox);
+				continue;
+			}
 			row.append(box);
 		}
 		$('.graph').append(row);
